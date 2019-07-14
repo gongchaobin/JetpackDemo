c66bc9bf8b6f983971011c1bd122965ab0be5ea1
gongChaobin <984519762@qq.com>
2019-07-14 20:27:00 +0800

    测试异步操作

--- app/src/main/java/com/example/android/roomwordssample/TestActivity.java
+++ app/src/main/java/com/example/android/roomwordssample/TestActivity.java
@@ -1,8 +1,6 @@
 package com.example.android.roomwordssample;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,34 +18,52 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final UserDao userDao = UserRoomDatabase.getDatabase(this).getUserDao();
+//        final UserDao userDao = UserRoomDatabase.getDatabase(this).getUserDao();
+//
+//
+//        User user = new User();
+//        user.uid  = 11;
+//        user.firstName = "gong";
+//        user.lastName = "cb";
+//        user.age = 23;
+//
+//        new InsertUserTask(userDao).execute(user);
 
 
-        User user = new User();
-        user.uid  = 11;
-        user.firstName = "gong";
-        user.lastName = "cb";
-        user.age = 23;
-
-        new InsertUserTask(userDao).execute(user);
+//        new AsyncTask<Void,Void,Void>() {
+//
+//            @Override
+//            protected Void doInBackground(Void... voids) {
+//                User user = new User();
+//                user.uid  = 12;
+//                user.firstName = "gong";
+//                user.lastName = "cb";
+//                user.age = 23;
+//
+//                UserDao userDao = UserRoomDatabase.getDatabase(TestActivity.this).getUserDao();
+//                long along = userDao.insert(user);
+//                Log.i("json","along: " + along);
+//
+//                return null;
+//            }
+//        }.execute();
     }
 
 
-    class InsertUserTask extends AsyncTask<User,Void,Void> {
-
-        private UserDao mUserDao;
-
-        public InsertUserTask(UserDao userDao) {
-            mUserDao = userDao;
-        }
-
-        @Override
-        protected Void doInBackground(User... users) {
-            Long along = mUserDao.insert(users[0]);
-            Log.i("json","along: " + along);
-            return null;
-        }
-    }
+//    class InsertUserTask extends AsyncTask<User,Void,Void> {
+//
+//        private UserDao mUserDao;
+//
+//        public InsertUserTask(UserDao userDao) {
+//            mUserDao = userDao;
+//        }
+//
+//        @Override
+//        protected Void doInBackground(User... users) {
+//            Long along = mUserDao.insert(users[0]);
+//            return null;
+//        }
+//    }
 
 
 }
